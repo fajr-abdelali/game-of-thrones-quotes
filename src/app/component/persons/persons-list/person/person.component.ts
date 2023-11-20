bff6de94f6923f44ef3fef4dcaba6fb4a1e0c9af
abdelali fajr <fajr.abdelali@outlook.com>
2023-11-20 22:25:31 +0100

    add button for get new quotes for character

--- src/app/component/persons/persons-list/person/person.component.ts
+++ src/app/component/persons/persons-list/person/person.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Character } from 'src/app/interface/GameOfThrones.interface';
+import { Character, Quote } from 'src/app/interface/GameOfThrones.interface';
 import { PersonsService } from 'src/app/service/persons.service';
+import { QuoteService } from 'src/app/service/quote.service';
 
 @Component({
   selector: 'app-person',
@@ -12,12 +13,21 @@ export class PersonComponent implements OnInit {
   character!: Character;
   slug: string = '';
 
-  constructor(private personsService: PersonsService, private router: ActivatedRoute) { }
+  constructor(private personsService: PersonsService,private quotesService:QuoteService, private router: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.slug = this.router.snapshot.params['slug'];
     console.log(this.slug)
     this.personsService.getPersonBySlug(this.slug).subscribe(characetr => this.character = characetr[0])
+  }
+
+  loadMoreQuotes() {
+    console.log(this.character);
+    this.quotesService.getQuotes().subscribe((quotes: Quote[]) => {
+      // Update the quotes array with the new quotes
+      console.log(quotes);
+      this.character.quotes = quotes.map(q => q.sentence);
+    });
   }
 
 }
